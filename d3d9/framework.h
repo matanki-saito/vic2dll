a49cc6959fa449c149551906255cdf3eebeeb8ca
matanki.saito <matanki.saito@gmail.com>
2021-06-26 05:22:37 +0900

    d3d9.dll

---
+++ d3d9/framework.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+// Windows ヘッダー ファイル
+#include <windows.h>
+#include <KnownFolders.h>
+#include <filesystem>
+#include <ShlObj.h>
+#include <Objbase.h>
