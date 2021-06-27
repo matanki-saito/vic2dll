d58efbb8cc37d86b3d6db3a56261a88cc8def0af
matanki.saito <matanki.saito@gmail.com>
2021-06-28 08:09:32 +0900

    改行処理

--- plugin/main_text.cpp
+++ plugin/main_text.cpp
@@ -4,8 +4,15 @@
 namespace MainText {
 	extern "C" {
 		void mainTextProc1();
+		void mainTextProc3();
+		void mainTextProc4();
 		uintptr_t maintTextProc1SrcAddress;
 		uintptr_t maintTextProc1ReturnAddress;
+		uintptr_t maintTextProc2SrcAddress;
+		uintptr_t maintTextProc2DstAddress;
+		uintptr_t maintTextProc3ReturnAddress;
+		uintptr_t maintTextProc4ReturnAddress1;
+		uintptr_t maintTextProc4ReturnAddress2;
 	}
 
 	DllError maintTextProc1Injector(RunOptions options) {
@@ -36,10 +43,105 @@ namespace MainText {
 		return e;
 	}
 
+	DllError maintTextProc2Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_0_4_0:
+			// mov cl, byte_xxxxx[eax]
+			BytePattern::temp_instance().find_pattern("8A 88 ? ? ? ? 88 8E ? ? ? ? 46 80");
+			if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の文字コピー処理")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				maintTextProc2SrcAddress = Injector::ReadMemory<uint32_t>(address + 2);
+				maintTextProc2DstAddress = Injector::ReadMemory<uint32_t>(address + 8);
+			}
+			else {
+				e.unmatch.mainTextProc2Injector = true;
+			}
+			break;
+		default:
+			e.version.mainTextProc2Injector = true;
+		}
+
+		return e;
+	}
+
+
+	DllError maintTextProc3Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_0_4_0:
+			// movzx ecx, cl
+			BytePattern::temp_instance().find_pattern("0F B6 C9 8B 8C 8B 94 00 00 00 85 C9");
+			if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の文字取得処理")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// jz loc_xxxxx
+				maintTextProc3ReturnAddress = address + 0xC;
+
+				Injector::MakeJMP(address, mainTextProc3, true);
+			}
+			else {
+				e.unmatch.mainTextProc3Injector = true;
+			}
+			break;
+		default:
+			e.version.mainTextProc3Injector = true;
+		}
+
+		return e;
+	}
+
+	DllError maintTextProc4Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_0_4_0:
+			// movzx ecx, cl
+			BytePattern::temp_instance().find_pattern("8B 45 24 03 C0 03 44 24 10 3B 44 24 28");
+			if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の改行処理戻り先")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// mov	eax, [ebp+arg_1C]
+				maintTextProc4ReturnAddress2 = address;
+			}
+			else {
+				e.unmatch.mainTextProc4Injector = true;
+			}
+
+			// movzx ecx, cl
+			BytePattern::temp_instance().find_pattern("83 7C 24 48 00 0F 85 2F 01 00 00");
+			if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の改行処理")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// jnz loc_xxxxx
+				maintTextProc4ReturnAddress1 = address + 5;
+
+				Injector::MakeJMP(address, mainTextProc4, true);
+			}
+			else {
+				e.unmatch.mainTextProc4Injector = true;
+			}
+			break;
+		default:
+			e.version.mainTextProc4Injector = true;
+		}
+
+		return e;
+	}
+
 	DllError Init(RunOptions options) {
 		DllError result = {};
 
 		result |= maintTextProc1Injector(options);
+
+		result |= maintTextProc2Injector(options);
+
+		result |= maintTextProc3Injector(options);
+
+		result |= maintTextProc4Injector(options);
 
 		return result;
 	}
--- plugin/main_text_asm.asm
+++ plugin/main_text_asm.asm
@@ -1,5 +1,10 @@
 EXTERN _maintTextProc1SrcAddress: DWORD
 EXTERN _maintTextProc1ReturnAddress: DWORD
+EXTERN _maintTextProc2SrcAddress: DWORD
+EXTERN _maintTextProc2DstAddress: DWORD
+EXTERN _maintTextProc3ReturnAddress: DWORD
+EXTERN _maintTextProc4ReturnAddress1: DWORD
+EXTERN _maintTextProc4ReturnAddress2: DWORD
 
 ESCAPE_SEQ_1	=	10h
 ESCAPE_SEQ_2	=	11h
@@ -14,6 +19,9 @@ NO_FONT			=	98Fh
 NOT_DEF			=	2026h
 
 .MODEL FLAT, C
+
+.DATA
+doubleByteFlag	DD		0
 
 .CODE
 mainTextProc1 PROC
@@ -51,6 +59,7 @@ JMP_D:
 	add		ecx, SHIFT_4;
 
 JMP_F:
+	mov		doubleByteFlag, 1
 	movzx	ecx, cx;
 	mov		edi, [esp + 530h - 514h]
 	add		edi, 2;
@@ -66,4 +75,81 @@ JMP_E:
 	push	_maintTextProc1ReturnAddress
 	ret
 mainTextProc1 ENDP
+
+;---------------------------------------------;
+
+mainTextProc3 PROC
+	mov		ecx, [_maintTextProc2SrcAddress]
+	add		eax, ecx
+
+	cmp		byte ptr[eax], ESCAPE_SEQ_1;
+	jz		JMP_A;
+	cmp		byte ptr[eax], ESCAPE_SEQ_2;
+	jz		JMP_B;
+	cmp		byte ptr[eax], ESCAPE_SEQ_3;
+	jz		JMP_C;
+	cmp		byte ptr[eax], ESCAPE_SEQ_4;
+	jz		JMP_D;
+	movzx	ecx, byte ptr[eax]
+	jmp		JMP_E;
+
+JMP_A:
+	movzx	ecx, word ptr[eax + 1];
+	jmp		JMP_F;
+
+JMP_B:
+	movzx	ecx, word ptr[eax + 1];
+	sub		ecx, SHIFT_2;
+	jmp		JMP_F;
+
+JMP_C:
+	movzx	ecx, word ptr[eax + 1];
+	add		ecx, SHIFT_3;
+	jmp		JMP_F;
+
+JMP_D:
+	movzx	ecx, word ptr[eax + 1];
+	add		ecx, SHIFT_4;
+
+JMP_F:
+	movzx	ecx, cx;
+	mov		edi, [esp + 530h - 514h]
+	add		edi, 2;
+	mov		[esp + 530h - 514h], edi
+	cmp		ecx, NO_FONT;
+	ja		JMP_G;
+	mov		ecx, NOT_DEF;
+
+JMP_G:
+	; copy character
+	mov		ax, word ptr[eax + 1];
+	mov		edi, [_maintTextProc2DstAddress]
+	add		edi, esi
+	mov		word ptr[edi], ax
+	add		esi,2
+
+JMP_E:
+	mov		ecx, [ebx+ecx*4+94h]
+	test	ecx, ecx
+
+	push	_maintTextProc3ReturnAddress
+	ret
+mainTextProc3 ENDP
+
+;---------------------------------------------;
+
+mainTextProc4 PROC
+	cmp		doubleByteFlag, 1
+	jz		JMP_A
+
+	cmp		dword ptr [esp + 530h - 4E8h], 0
+	mov		doubleByteFlag, 0
+	push	_maintTextProc4ReturnAddress1
+	ret
+
+JMP_A:
+	mov		doubleByteFlag, 0
+	push	_maintTextProc4ReturnAddress2
+	ret
+mainTextProc4 ENDP
 END
