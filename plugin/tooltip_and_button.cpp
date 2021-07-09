c91f30f2c75f57fa8758369a49d3fbfdfdbdb93a
matanki.saito <matanki.saito@gmail.com>
2021-07-09 20:59:07 +0900

    Tooltip

--- plugin/tooltip_and_button.cpp
+++ plugin/tooltip_and_button.cpp
@@ -7,6 +7,11 @@ namespace TooltipAndButton {
 		uintptr_t tooltipAndButtonProc1ReturnAddress;
 		void tooltipAndButtonProc2();
 		uintptr_t tooltipAndButtonProc2ReturnAddress;
+		void tooltipAndButtonProc3();
+		uintptr_t tooltipAndButtonProc3ReturnAddress;
+		void tooltipAndButtonProc4();
+		uintptr_t tooltipAndButtonProc4ReturnAddress;
+		uintptr_t tooltipAndButtonProc4CallAddress;
 	}
 
 	DllError tooltipAndButtonProc1Injector(RunOptions options) {
@@ -38,11 +43,12 @@ namespace TooltipAndButton {
 	DllError tooltipAndButtonProc2Injector(RunOptions options) {
 		DllError e = {};
 
+		// 別のルーチンにある
 		switch (options.version) {
 		case v3_0_4_0:
 			// movzx   eax, byte ptr [eax+esi]
 			BytePattern::temp_instance().find_pattern("0F B6 04 30 8B BC 87 94 00 00 00");
-			if (BytePattern::temp_instance().has_size(1, u8"?")) {
+			if (BytePattern::temp_instance().has_size(1, u8"改行のためのカウント処理")) {
 				uintptr_t address = BytePattern::temp_instance().get_first().address();
 
 				// jnz loc
@@ -61,12 +67,71 @@ namespace TooltipAndButton {
 		return e;
 	}
 
+	DllError tooltipAndButtonProc3Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_0_4_0:
+			// movzx   eax, byte ptr [eax+esi]
+			BytePattern::temp_instance().find_pattern("0F B6 04 30 8B 84 87 94 00 00 00 85 C0 0F 84 2F 01 00 00");
+			if (BytePattern::temp_instance().has_size(1, u8"ループ１の文字取得処理")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// jz loc
+				tooltipAndButtonProc3ReturnAddress = address + 0xD;
+
+				Injector::MakeJMP(address, tooltipAndButtonProc3, true);
+			}
+			else {
+				e.unmatch.tooltipAndButtonProc3Injector = true;
+			}
+			break;
+		default:
+			e.version.tooltipAndButtonProc3Injector = true;
+		}
+
+		return e;
+	}
+
+	DllError tooltipAndButtonProc4Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_0_4_0:
+			// mov     cl, [eax+esi]
+			BytePattern::temp_instance().find_pattern("8A 0C 30 88 8C 24 90 00 00 00");
+			if (BytePattern::temp_instance().has_size(1, u8"ループ１の文字コピー処理")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// push 0FFFFFFFFh
+				tooltipAndButtonProc4ReturnAddress = address + 0x46;
+
+				// call xxxxx
+				tooltipAndButtonProc4CallAddress = Injector::GetBranchDestination(address + 0x41).as_int();
+
+				Injector::MakeJMP(address, tooltipAndButtonProc4, true);
+			}
+			else {
+				e.unmatch.tooltipAndButtonProc4Injector = true;
+			}
+			break;
+		default:
+			e.version.tooltipAndButtonProc4Injector = true;
+		}
+
+		return e;
+	}
+
 	DllError Init(RunOptions options) {
 		DllError result = {};
 
 		result |= tooltipAndButtonProc1Injector(options);
 
 		result |= tooltipAndButtonProc2Injector(options);
+
+		result |= tooltipAndButtonProc3Injector(options);
+
+		result |= tooltipAndButtonProc4Injector(options);
 
 		return result;
 	}
--- plugin/tooltip_and_button_asm.asm
+++ plugin/tooltip_and_button_asm.asm
@@ -1,5 +1,8 @@
 EXTERN _tooltipAndButtonProc1ReturnAddress: DWORD
 EXTERN _tooltipAndButtonProc2ReturnAddress: DWORD
+EXTERN _tooltipAndButtonProc3ReturnAddress: DWORD
+EXTERN _tooltipAndButtonProc4ReturnAddress: DWORD
+EXTERN _tooltipAndButtonProc4CallAddress: DWORD
 
 ESCAPE_SEQ_1	=	10h
 ESCAPE_SEQ_2	=	11h
@@ -14,6 +17,9 @@ NO_FONT			=	98Fh
 NOT_DEF			=	2026h
 
 .MODEL FLAT, C
+
+.DATA
+tmp1	DW		0
 
 .CODE
 tooltipAndButtonProc1 PROC
@@ -113,5 +119,104 @@ JMP_E:
 	push	_tooltipAndButtonProc2ReturnAddress
 	ret
 tooltipAndButtonProc2 ENDP
+
+
+;------------------------------;
+
+tooltipAndButtonProc3 PROC
+
+	cmp		byte ptr[eax + esi], ESCAPE_SEQ_1;
+	jz		JMP_A;
+	cmp		byte ptr[eax + esi], ESCAPE_SEQ_2;
+	jz		JMP_B;
+	cmp		byte ptr[eax + esi], ESCAPE_SEQ_3;
+	jz		JMP_C;
+	cmp		byte ptr[eax + esi], ESCAPE_SEQ_4;
+	jz		JMP_D;
+	movzx	eax, byte ptr [eax + esi];
+	jmp		JMP_E;
+
+JMP_A:
+	movzx	eax, word ptr[eax + esi + 1];
+	jmp		JMP_F;
+
+JMP_B:
+	movzx	eax, word ptr[eax + esi + 1];
+	sub		eax, SHIFT_2;
+	jmp		JMP_F;
+
+JMP_C:
+	movzx	eax, word ptr[eax + esi + 1];
+	add		eax, SHIFT_3;
+	jmp		JMP_F;
+
+JMP_D:
+	movzx	eax, word ptr[eax + esi + 1];
+	add		eax, SHIFT_4;
+
+JMP_F:
+	movzx	eax, ax;
+	add		esi, 2;
+	cmp		eax, NO_FONT;
+
+	ja		JMP_E;
+	mov		eax, NOT_DEF;
+
+JMP_E:
+	
+	mov		eax, [edi+eax*4 + 94h]
+	test	eax, eax
+	push	_tooltipAndButtonProc3ReturnAddress
+	ret
+tooltipAndButtonProc3 ENDP
+
+;------------------------------;
+
+tooltipAndButtonProc4 PROC
+
+	mov     cl, [eax+esi]
+	mov     byte ptr [esp + 400h - 370h], cl
+	mov     dword ptr [esp + 400h - 33Ch], 0Fh
+	mov     dword ptr [esp + 400h - 340h], 0
+	mov     byte ptr [esp + 400h - 350h], 0
+	mov     edx, [esp + 400h - 370h]
+	push    edx
+
+	cmp		cl, ESCAPE_SEQ_1;
+	jz		JMP_A;
+	cmp		cl, ESCAPE_SEQ_2;
+	jz		JMP_A;
+	cmp		cl, ESCAPE_SEQ_3;
+	jz		JMP_A;
+	cmp		cl, ESCAPE_SEQ_4;
+	jz		JMP_A;
+	jmp		JMP_B;
+
+JMP_A:
+	push	3
+	mov		ax, word ptr [eax+esi+1]
+	mov		tmp1, ax
+	jmp		JMP_C
+JMP_B:
+	mov		tmp1, 0
+	push	1
+
+JMP_C:
+	lea     ecx, [esp + 408h - 350h]
+	mov     byte ptr [esp + 408h - 4h], 3
+	call	_tooltipAndButtonProc4CallAddress
+
+	cmp		tmp1, 0
+	jz		JMP_D
+
+	mov		cx, tmp1
+	mov		word ptr [esp + 400h - 350h + 1], cx
+	lea		ecx, [esp + 400h - 350h]
+
+JMP_D:
+	push	_tooltipAndButtonProc4ReturnAddress
+	ret
+
+tooltipAndButtonProc4 ENDP
 
 END
